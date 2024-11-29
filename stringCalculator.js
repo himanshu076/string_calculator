84101416ff8e04ed1aef8bd281bf22fc81b83722
himanshu <himanshu.andel@yahoo.in>
2024-11-29 23:52:19 +0530

    string contains a single number

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,6 +2,10 @@ function add(numbers) {
   if (numbers === '') {
     return 0;
   }
+
+  // Split the string by commas and add the numbers
+  const numArray = numbers.split(/[\n,]+/);
+  return numArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
 
 module.exports = { add };
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -3,3 +3,11 @@ const { add } = require('./stringCalculator');
 test('returns 0 for an empty string', () => {
   expect(add('')).toBe(0);
 });
+
+test('returns the number for a single number', () => {
+  expect(add('1')).toBe(1);
+});
+
+test('returns the sum for two numbers', () => {
+  expect(add('1,5')).toBe(6);
+});
