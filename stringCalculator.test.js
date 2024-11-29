7f9b9978c0a44df67f496e48299f0d066b0e2343
himanshu <himanshu.andel@yahoo.in>
2024-11-29 23:53:42 +0530

    numbers should separated by new lines

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -11,3 +11,7 @@ test('returns the number for a single number', () => {
 test('returns the sum for two numbers', () => {
   expect(add('1,5')).toBe(6);
 });
+
+test('returns the sum for numbers with new lines', () => {
+  expect(add('1\n2,3')).toBe(6);
+});
